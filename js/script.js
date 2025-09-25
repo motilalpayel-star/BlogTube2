b4eeea67cdf1101bd448be5a36b4cd600d286706
motilalpayel-star <motilal.payel@gmail.com>
2025-09-25 09:17:15 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
-const params = new URLSearchParams(window.location.search);
-const cat = (params.get('cat') || 'all').toLowerCase();
+// Functionality for Category Pages
+const currentCategory = document.getElementById('current-category');
 
-document.getElementById('current-category').textContent = 
-    cat === 'all' ? 'All' : cat.replace(/-/g,' ');
+if (currentCategory) {
+    const params = new URLSearchParams(window.location.search);
+    const cat = (params.get('cat') || 'all').toLowerCase();
+    currentCategory.textContent = cat === 'all' ? 'All' : cat.replace(/-/g, ' ');
 
-document.querySelectorAll('.feature-box').forEach(box=>{
-   const boxCat = box.dataset.category.toLowerCase();
-   if (cat === 'all' || boxCat === cat){
-       box.style.display = 'block';
-   } else {
-       box.style.display = 'none';
-   }
+    document.querySelectorAll('.feature-box').forEach(box => {
+        const boxCat = box.dataset.category.toLowerCase();
+        if (cat === 'all' || boxCat === cat) {
+            box.style.display = 'block';
+        } else {
+            box.style.display = 'none';
+        }
+    });
+}
+
+// Functionality for Mobile Menu Icon
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.navbar ul');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
 });
+    
